421ec3c6ab9efd224390617ea25aec434bbd189c
Integrated Bio <117644708+IntegratedBio@users.noreply.github.com>
2023-01-02 07:33:05 -0500

    Add files via upload

---
+++ driver_init.c
@@ -0,0 +1,179 @@
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+#include <hpl_gclk_base.h>
+#include <hpl_pm_base.h>
+
+#include <hpl_adc_base.h>
+
+struct adc_sync_descriptor ADC_0;
+
+struct usart_sync_descriptor CONSOLE_UART;
+
+void ADC_0_PORT_init(void){
+	gpio_set_pin_direction(PB03, GPIO_DIRECTION_OFF);
+	gpio_set_pin_function(PB03, PINMUX_PB03B_ADC_AIN11);
+}
+
+void ADC_0_CLOCK_init(void){
+	_pm_enable_bus_clock(PM_BUS_APBC, ADC);
+	_gclk_enable_channel(ADC_GCLK_ID, CONF_GCLK_ADC_SRC);
+}
+
+void ADC_0_init(void){
+	ADC_0_CLOCK_init();
+	ADC_0_PORT_init();
+	adc_sync_init(&ADC_0, ADC, (void *)NULL);
+}
+
+void CONSOLE_UART_PORT_init(void){
+	//PORT A THROUGH THE USB
+	//gpio_set_pin_function(PA24, PINMUX_PA24D_SERCOM5_PAD2);					//GPIO 'TX' PIN CONFIGURATION WITH PB23 PORT DEFINITION FOR SERCOM5 PERIPHERAL
+	//gpio_set_pin_function(PA25, PINMUX_PA25D_SERCOM5_PAD3);					//GPIO 'RX' PIN CONFIGURATION WITH PB22 PORT DEFINITION FOR SERCOM5 PERIPHERAL
+	
+	//PORT B THROUGH THE TX/RX Pins
+	gpio_set_pin_function(PB22, PINMUX_PB22D_SERCOM5_PAD2);						//GPIO 'TX' PIN CONFIGURATION WITH PB23 PORT DEFINITION FOR SERCOM5 PERIPHERAL
+	gpio_set_pin_function(PB23, PINMUX_PB23D_SERCOM5_PAD3);						//GPIO 'RX' PIN CONFIGURATION WITH PB22 PORT DEFINITION FOR SERCOM5 PERIPHERAL
+}
+
+void CONSOLE_UART_CLOCK_init(void){
+	_pm_enable_bus_clock(PM_BUS_APBC, SERCOM5);									//POWER MANAGEMENT BUS APB BASE ADDRESS TO SERCOM5 
+	_gclk_enable_channel(SERCOM5_GCLK_ID_CORE, CONF_GCLK_SERCOM5_CORE_SRC);		//PORT DEFINITION FOR SERCOM5 PERIPHERAL INSTANCE SOURCED W/ GCLK:0
+}
+
+void CONSOLE_UART_init(void){
+	CONSOLE_UART_CLOCK_init();
+	usart_sync_init(&CONSOLE_UART, SERCOM5, (void *)NULL);
+	CONSOLE_UART_PORT_init();
+}
+
+void stdio_redirect_init(void){
+	usart_sync_enable(&CONSOLE_UART);
+	stdio_io_init(&CONSOLE_UART.io);
+}
+
+void delay_driver_init(void){
+	delay_init(SysTick);
+}
+
+void system_init(void){
+	init_mcu();
+	ADC_0_init();
+	CONSOLE_UART_init();
+	delay_driver_init();
+	LED_0_init();
+	PUMP_init();
+	VALVE_0_init();
+	VALVE_1_init();
+	LOW_LVL_init();
+	ALARM_init();
+}
+
+///////////////////////////////////////////
+// BUILT-IN LED INITIALIZATION
+///////////////////////////////////////////
+void LED_0_init(void){	//CONFIGURE GPIO ON PA17 TO FUNCTION AS ONBOARD LED INDICATOR
+	gpio_set_pin_direction(LED_BUILTIN, GPIO_DIRECTION_OUT);
+	gpio_set_pin_pull_mode(LED_BUILTIN, GPIO_PULL_OFF);
+	gpio_set_pin_function(LED_BUILTIN, GPIO_PIN_FUNCTION_OFF);
+	gpio_set_pin_level(LED_BUILTIN,true);
+}
+
+void LED_0_on(void){
+	gpio_set_pin_level(LED_BUILTIN,true);
+}
+void LED_0_off(void){
+	gpio_set_pin_level(LED_BUILTIN,false);
+}
+
+void LED_0_toggle(void){
+	gpio_toggle_pin_level(LED_BUILTIN);
+}
+
+///////////////////////////////////////////
+// PUMP DRIVER INTILIZATION
+///////////////////////////////////////////
+
+void PUMP_init(void){
+	//SET PIN DIRECTION (3 - Different expressions)
+	gpio_set_pin_direction(PUMP_0_PIN, GPIO_DIRECTION_OUT);
+	//SET PIN FUNCTION
+	gpio_set_pin_function(PUMP_0_PIN, GPIO_PIN_FUNCTION_OFF);
+	//CONFIGURE GPIO TO FUNCTION AS A RELAY OUTPUT FOR THE PUMP
+	gpio_set_pin_level(PUMP_0_PIN,true);
+}
+void PUMP_off(void){
+	gpio_set_pin_level(PUMP_0_PIN, true);//stop current when high
+	printf("\t Pump Off");
+};
+void PUMP_on(void){
+	gpio_set_pin_level(PUMP_0_PIN,false);//sink current when low
+	printf("\t Pump On");
+};
+
+///////////////////////////////////////////
+// VALVE DRIVER INTILIZATION
+///////////////////////////////////////////
+void VALVE_0_init(void){
+	gpio_set_pin_direction(VALVE_0_PIN, GPIO_DIRECTION_OUT);	//SET PIN AS AN OUTPUT ->
+	gpio_set_pin_function(VALVE_0_PIN, GPIO_PIN_FUNCTION_OFF);	//SET PIN FUNCTION OFF
+	gpio_set_pin_level(VALVE_0_PIN,true);						//START IN THE OFF POSITION -> PULL DOWN TO ALARM
+}
+
+void VALVE_0_off(void){
+	gpio_set_pin_level(VALVE_0_PIN, true);						//CYCLE OFF
+	printf("\t Valve 0 Off");
+}
+
+void VALVE_0_on(void){
+	gpio_set_pin_level(VALVE_0_PIN, false);						//CYCLE ON
+	printf("\t Valve 0 On");
+}
+
+void VALVE_1_init(void){
+	gpio_set_pin_direction(VALVE_1_PIN, GPIO_DIRECTION_OUT);	//SET PIN AS AN OUTPUT ->
+	gpio_set_pin_function(VALVE_1_PIN, GPIO_PIN_FUNCTION_OFF);	//SET PIN FUNCTION OFF
+	gpio_set_pin_level(VALVE_1_PIN,true);						//START IN THE OFF POSITION -> PULL DOWN TO ALARM
+}
+
+void VALVE_1_off(void){	
+	gpio_set_pin_level(VALVE_1_PIN, true);						//SET PIN HIGH TO STOP SINKING
+	printf("\t Valve 1 Off");
+}
+
+void VALVE_1_on(void){
+	gpio_set_pin_level(VALVE_1_PIN, false);						//SET PIN LOW TO START SINKING
+	printf("\t Valve 1 On");
+}
+
+///////////////////////////////////////////
+// LOW LEVEL SWITCH DRIVER INITIALIZATION
+///////////////////////////////////////////
+void LOW_LVL_init(void){
+	gpio_set_pin_direction(LOW_LVL_PIN, GPIO_DIRECTION_IN);		//SET PIN AS A A DIGITAL INPUT
+	gpio_set_pin_pull_mode(LOW_LVL_PIN,GPIO_PULL_UP);			//PULLDOWN RESISTOR TO REDUCE CURRENT INTO THE BOARD
+	gpio_set_pin_function(LOW_LVL_PIN, GPIO_PIN_FUNCTION_OFF);		
+}
+
+// LOW LEVEL SWITCH CALLBACK FUNCTION - TBD ASSING INTERRUPT
+void LOW_LVL_cb(void){}
+
+///////////////////////////////////////////
+// ALARM DRIVER INTILIZATION
+///////////////////////////////////////////
+void ALARM_init(void){
+	gpio_set_pin_direction(ALARM_PIN, GPIO_DIRECTION_OUT);		//SET PIN AS AN OUTPUT ->
+	gpio_set_pin_function(ALARM_PIN, GPIO_PIN_FUNCTION_OFF);	//SET PIN FUNCTION OFF
+	gpio_set_pin_level(ALARM_PIN,true);							//START IN THE OFF POSITION -> PULL DOWN TO ALARM
+}
+
+void ALARM_off(void){
+	gpio_set_pin_level(ALARM_PIN, true);						//SET PIN HIGH TO STOP SINKING
+	printf("\t Alarm Off");
+}
+
+void ALARM_on(void){
+	gpio_set_pin_level(ALARM_PIN, false);						//SET PIN LOW TO START SINKING
+	printf("\t Alarm On");
+}
---
+++ main.c
@@ -0,0 +1,150 @@
+/* File:	main.c
+ * Author:	Integrated Bioengineering, LLC.
+ * Processor: SAMD21G18A @ 48MHz, 3.3v
+ * Program: Main Application File
+ * Compiler: ARM-GCC Microchip Studio
+ * Program Version 1.0
+ * Program Description: Pump Control Application
+ * Hardware Description: Pressure Transmitter and pump relay
+ * 
+ * Change History:
+ * Author                Rev     Date          Description
+ * Brad				     1.0     02/12/2022    ADC Setup
+ */
+
+// DEFINES AND INCLUDES
+#include <atmel_start.h>
+
+#define ADC_11_channel					11				//A7 Labeled on Board
+#define ADC_11_raw_buffer_size			2				//Bytes
+#define ADC_11_ave_buffer_size			5				//
+#define ADC_11_voltage_reference		2.229729729730	//VDC INTERNAL
+
+//PRESSURE MAPPING VALUES
+//ADC VALUES
+#define ADC_11_fsr		65520 	//FSR, HIGHEST VALYE ADC WILL READOUT. 4 BITS ARE DISCARDED AND USED INSTEAD AS MOST SIGNIFICANT BITS (MSB)
+#define ADC_11_max		64638	//CALCULATED 2.20/2.23 * FSR VDC 
+#define ADC_11_min		12927	//CALCULATED 0.44/2.23 * VDC 
+
+/* TRAFAG INDSUTRIAL PRESSURE TRANSMITTER
+TYPE:			8287.G8.2530
+S/N:			832054-003 | 08/21
+RANGE:			0....150 psi-G | max. 450 psi
+POWER:			+9 - 32 VDC
+OUTPUT:			4-20 ma converted to voltage with a shunt 110 Ohm 1/4 Watt resistor [YR1B110RCC] to ground
+*/
+#define voltage_at_max	2.20	// VDC	(2.20 Calculated @ 20ma | ~	150 PSI)
+#define voltage_at_min	0.44	// VDC	(0.44 Calculated at 4ma	| ~	0  PSI)
+#define min_pressure	0
+#define max_pressure	150
+
+
+//GLOBAL VARIABLES
+uint16_t	ADC_11_value	=	0;	// 16 BIT UNSIGNED INTEGER FOR SINGLE ENDED (ALWAYS POSITVE)
+int values;							// INTEADC VALUES THAT WILL BE
+int size;
+
+float		ADC_11_volts		=	0;	
+float		ADC_11_pressure		=	0;		
+int			ADC_11_pressure_int =	0;
+uint8_t		set_pressure		=	100;	// PRESSURE SETPOINT
+
+float map(float x, float in_min, float in_max, float out_min, float out_max) { return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; }
+
+bool pump_state     =	0;     /*!< flag to represent the application state*/
+bool Low_level_state =  0;
+int main(void){
+	atmel_start_init();
+	adc_sync_enable_channel(&ADC_0, ADC_11_channel);
+
+	char *setup_greeting = "\nSETUP COMPLETE!\n";	
+	printf("\n%s", setup_greeting); //GREETING
+	
+	///////////////////////////END OF SETUP////////////////////////////////
+	
+	while (1)
+	{
+		do {
+			ADC_11_read();
+			
+			
+			if (pump_state == false){					//if pump is off, see if you need to turn it on
+				if(ADC_11_pressure < set_pressure){		//CHECK PRESSURE
+					PUMP_on();
+					pump_state = true;
+				}
+			}		
+		
+			if (pump_state == true){					//IF the pump is not off, see if you need to turn it off
+				if(ADC_11_pressure >= set_pressure){
+					PUMP_off();
+					pump_state = false;
+				}
+			}
+	
+			delay_ms(25);								
+		} while (gpio_get_pin_level(LOW_LVL_PIN));
+		
+		do {
+		delay_ms(100);
+		} while (!gpio_get_pin_level(LOW_LVL_PIN));
+		
+	}//END OF APP LOOP
+	
+}//END OF MAIN
+
+//************************************************************************
+	void ADC_11_read(void){
+		/* ///////////////////////////////////////////////////////////////
+		 * ADC READ CONFIGURATION SUMMARY
+		 * ADC CHANNEL:			11 (+) PB03/A7 
+		 * ADC MODE:			SINGLE ENDED VS. INTERNAL GROUND GROUND	
+		 * ADC CLOCK SOURCE:	GENERIC CLOCK 2 >> [OSC32K] HIGH-ACCURACY
+		 * RESOLUTION:			16 BIT
+		 * AVERAGED SAMPLES:	16
+		 * REFERENCE:			1 / 1.48 VDDANA reference [2.23 VDC]
+		 *////////////////////////////////////////////////////////////////
+		LED_0_on();
+		uint8_t ADC_11_raw_buffer[ADC_11_raw_buffer_size];						//Initializing an ADC buffer for raw outputs
+		int ADC_11_ave_buffer[ADC_11_ave_buffer_size];							//Initializing an averaging buffer for the raw values	
+
+		for (int i = 0; i < ADC_11_ave_buffer_size; i++){
+			adc_sync_read_channel(&ADC_0, ADC_11_channel, ADC_11_raw_buffer, ADC_11_raw_buffer_size);
+			ADC_11_value = ADC_11_raw_buffer[1];								//See adcbuff value
+			ADC_11_value = ADC_11_value << 8;									//Moving to the upper 8 bits
+			ADC_11_value = ADC_11_value | ADC_11_raw_buffer[0];					//Appending lower 8 bit value to previous value			//16-bit ADC value:
+			//printf("\nADC 11 Value: %i",ADC_11_value);						//print integer readout to COM 1
+			ADC_11_ave_buffer[i] = ADC_11_value;
+			delay_ms(1);
+		}
+
+		//CALCULATING AVERAGE ADC CHANNEL 11 VALUE
+		ADC_11_value = average(ADC_11_ave_buffer, ADC_11_ave_buffer_size);
+		printf("\nADC 11:\t%i",ADC_11_value);
+		
+		ADC_11_volts = ((float)ADC_11_value / (float)ADC_11_fsr) * (float)ADC_11_voltage_reference;			//CALCULATING VOLTAGE READING FOR THE ADC 11
+		ADC_11_pressure = map(ADC_11_volts,voltage_at_min,voltage_at_max,min_pressure,max_pressure);		//CALCULATING PRESSURE READING FOR THE ADC 11
+		ADC_11_pressure_int = ADC_11_pressure;
+		
+		if (ADC_11_pressure_int <= 0){
+		ADC_11_pressure_int = 0;	
+		}
+				
+		char ADC_11_pressure_str_buffer[3] = {0};							//string buffer to print floats to 
+		gcvt(ADC_11_volts,3,ADC_11_pressure_str_buffer);					//convert float to string that is 3 characters
+		printf("\tVolts:\t%.3s", ADC_11_pressure_str_buffer);						//prints 3 characters of sting buffer
+		
+		//gcvt(ADC_11_pressure,3,ADC_11_pressure_int);						//string buffer to print floats to 
+		printf("\tPressure:\t%.3i psi", ADC_11_pressure_int);				//convert float to string that is 3 characters
+		
+		LED_0_off(); //Visually indicate the Red cycle has concluded
+	}	//END ADC_11_READ
+
+	//ADC ACERAGING FUNCTION
+	float average(int* values, int size){
+		int sum = 0;
+		for (int i = 0; i < size; i++){
+			sum += values[i];
+		}
+		return (float)sum / size;
+	}
